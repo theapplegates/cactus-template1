88eac3723cc21da0da0cf91ffaed21f0eaaab34b
Paul <paul@theapplegates.com>
2022-08-17 20:10:35 -0400

    Update site-meta.config.ts

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,9 +1,8 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
-  lang: "en-GB",
-  ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+  title: "Living LIfe",
+  description: "Follow my daily hijinks here. A lot of science-related stuff, and some serious subjects like cancer. Fuck cancer.
+  ogLocale: "en_US",
+  githubUrl: "https://cactus-wip.paulapplegate.com",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
 }
